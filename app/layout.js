e6e806bb9895fca096978effab4148d3ce031049
beejaytmg <tmgbijay869@gmail.com>
2024-08-27 06:40:38 +0545

    first commit

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,8 @@ import Footer from '../components/Footbar';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9631856282418168"
+     crossorigin="anonymous"></script>
       <body>
         <Navbar />
         <main className="min-h-screen">{children}</main>
